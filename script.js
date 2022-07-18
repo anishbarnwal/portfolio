ab7da1f8c258c6558b8f4f12607b06b0984fa8e6
anishbarnwal <101061991+anishbarnwal@users.noreply.github.com>
2022-07-18 19:12:18 +0530

    updates

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ $(document).ready(function(){
      //typing animation script
  
       var typed = new Typed(".typing",{
-         strings:["Full stack Web developer","Coder","Blogger"],
+         strings:["Full stack Web developer","Coder"],
          typeSpeed: 100,
          backSpeed: 60,
          loop:true
      })
  
      var typed = new Typed(".typing-2",{
-         strings:["Full stack Web developer","Coder","Blogger"],
+         strings:["Full stack Web developer","Coder"],
          typeSpeed: 100,
          backSpeed: 60,
          loop:true
